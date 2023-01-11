f4e72d0bafe5009c579e007bbd6cb21f3466691d
TStavale <tassiostavale@hotmail.com>
2023-01-10 22:21:51 -0300

    I included the values within the fields in the section and used a library to put the values into the country's currency format.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,19 @@ const dolar = 5.2;
 
 const convertValue = () => {
     const inputreais = document.getElementById('input-real').value;
+    const realValueText = document.getElementById('real-value-text');
+    const currencyValueText = document.getElementById('currency-value-text');
 
-    console.log(inputreais / dolar)
+    realValueText.innerHTML = inputreais;
+    
+    realValueText.innerHTML = new Intl.NumberFormat('pt-Br',
+        { style: 'currency', currency: 'BRL'}
+    ). format(inputreais)
+
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-Us',
+        { style: 'currency', currency: 'USD'}
+    ). format(inputreais / dolar)
+    
 };
 
 button.addEventListener('click', convertValue);
