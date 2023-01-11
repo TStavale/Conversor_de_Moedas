732fedfd241bd69da14ff4ca2dc876418eb6a037
TStavale <tassiostavale@hotmail.com>
2023-01-10 22:44:07 -0300

    I took the event of change a tag select so that when it occurs, it calls the function to detect which currency I want to convert and change the name of the currency and the country's flag.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const button = document.getElementById ('convert-button');
+const select = document.getElementById ('currency-select');
 
 const dolar = 5.2;
 
@@ -7,16 +8,33 @@ const convertValue = () => {
     const realValueText = document.getElementById('real-value-text');
     const currencyValueText = document.getElementById('currency-value-text');
 
-    realValueText.innerHTML = inputreais;
+    //realValueText.innerHTML = inputreais;
     
     realValueText.innerHTML = new Intl.NumberFormat('pt-Br',
         { style: 'currency', currency: 'BRL'}
-    ). format(inputreais)
+    ). format(inputreais);
 
     currencyValueText.innerHTML = new Intl.NumberFormat('en-Us',
         { style: 'currency', currency: 'USD'}
-    ). format(inputreais / dolar)
+    ). format(inputreais / dolar);
     
 };
 
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name');
+    const currencyImg = document.getElementById('currency-img');
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/img/euro.png"
+    }
+
+    if (select.value === 'U$$ Dólar americano') {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./assets/img/eua.png"
+    }
+
+};
+
 button.addEventListener('click', convertValue);
+select.addEventListener('change', changeCurrency);
