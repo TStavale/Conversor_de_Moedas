bd0d9f6797c5e12e141b7d2628c280a104183a43
TStavale <tassiostavale@hotmail.com>
2023-01-11 22:21:21 -0300

    This time I took the select tag change event so that when it occurs, call the function to convert the value of the real to the euro.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,31 @@ const button = document.getElementById ('convert-button');
 const select = document.getElementById ('currency-select');
 
 const dolar = 5.2;
+const euro = 5.9;
+
 
 const convertValue = () => {
     const inputreais = document.getElementById('input-real').value;
     const realValueText = document.getElementById('real-value-text');
     const currencyValueText = document.getElementById('currency-value-text');
 
-    //realValueText.innerHTML = inputreais;
     
     realValueText.innerHTML = new Intl.NumberFormat('pt-Br',
         { style: 'currency', currency: 'BRL'}
     ). format(inputreais);
 
-    currencyValueText.innerHTML = new Intl.NumberFormat('en-Us',
-        { style: 'currency', currency: 'USD'}
-    ). format(inputreais / dolar);
-    
+    if(select.value === 'U$$ Dólar americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-Us',
+            { style: 'currency', currency: 'USD'}
+        ). format(inputreais / dolar);
+    }
+
+    if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR'}
+        ). format(inputreais / euro);
+    }
+
 };
 
 changeCurrency = () => {
@@ -33,6 +42,8 @@ changeCurrency = () => {
         currencyName.innerHTML = "Dólar americano"
         currencyImg.src = "./assets/img/eua.png"
     }
+
+    convertValue()
 
 };
 
