c575613bafa3cfa9995bcda42a9e66965ebaa49c
TStavale <tassiostavale@hotmail.com>
2023-01-12 00:17:57 -0300

    This time I got the select tag change event so that when it happens, call the function to convert the value from real to bitcoin. Also, the background will change according to the currency selected for conversion.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const button = document.getElementById ('convert-button');
 const select = document.getElementById ('currency-select');
 
-const dolar = 5.2;
-const euro = 5.9;
+const dolar = 5.16;
+const euro = 5.55;
+const BTC = 94196.67;
 
 
 const convertValue = () => {
@@ -27,20 +28,35 @@ const convertValue = () => {
         ). format(inputreais / euro);
     }
 
+    if (select.value === 'Ƀ Bitcoin') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'BTC'}
+        ). format(inputreais / BTC);
+    }
+
 };
 
 changeCurrency = () => {
     const currencyName = document.getElementById('currency-name');
-    const currencyImg = document.getElementById('currency-img');
+    const currencyImg = document.getElementById('currency-img'); 
+    const currencyBackground = document.querySelector('body');
 
     if (select.value === '€ Euro') {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./assets/img/euro.png"
+        currencyBackground.style.backgroundImage = "url(./assets/img/euro_background.jpg)"
     }
 
     if (select.value === 'U$$ Dólar americano') {
         currencyName.innerHTML = "Dólar americano"
         currencyImg.src = "./assets/img/eua.png"
+        currencyBackground.style.backgroundImage = "url(./assets/img/dolar_background.jpg)"
+    }
+
+    if (select.value === 'Ƀ Bitcoin') {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/img/btc.png"
+        currencyBackground.style.backgroundImage = "url(./assets/img/btc_background.jpg)"
     }
 
     convertValue()
